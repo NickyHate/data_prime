4e4aa534c07898ff763d22457e1a2263fda678d2
Никита <nikitostokarev22@gmail.com>
2020-08-14 19:11:36 +0300

    draw new chart

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const canvas = document.getElementById('c1');
-
 const ctx = canvas.getContext('2d');
+const pi = Math.PI;
 
-// ctx.fillRect(x, y, width, height);
-ctx.fillStyle = 'red';
-ctx.fillRect(100, 50, 150, 75);
-
-// стереть всё 
-
-ctx.clearRect(0, 0, 400, 200);
-
-ctx.rect( 50, 50, 100, 100);
-ctx.strokeStyle = 'yellow';
-ctx.lineWidth = '10';
+ctx.strokeStyle = 'black';
+ctx.lineWidth = '2'
+ctx.moveTo( 50, 50 );
+ctx.lineTo( 50, 450 );
+ctx.moveTo( 50, 450 );
+ctx.lineTo( 750, 450 );
 ctx.stroke();
+ctx.beginPath();
+ctx.fillStyle = 'white'
+ctx.moveTo(100, 300);
+ctx.arc(100, 300, 5, 0, 2*pi, false);
+
+ctx.moveTo(100, 300);
+ctx.arc(200, 300, 5, 0, 2*pi, false);
+
+ctx.moveTo(200, 300);
+ctx.arc(300, 300, 5, 0, 2*pi, false);
+
+ctx.moveTo(300, 300);
+ctx.arc(400, 300, 5, 0, 2*pi, false);
+
+ctx.moveTo(400, 300);
+ctx.arc(500, 300, 5, 0, 2*pi, false);
+
+ctx.moveTo(500, 300);
+ctx.arc(600, 300, 5, 0, 2*pi, false);
+
+ctx.moveTo(600, 300);
+ctx.arc(700, 300, 5, 0, 2*pi, false);
+
+ctx.closePath();
+ctx.stroke();
+ctx.fill()
